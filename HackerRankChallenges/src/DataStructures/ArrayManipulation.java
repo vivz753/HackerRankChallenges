2d9d39c5ca0430c8c5b5b8967244caccb1dfd1f2
vivz753 <vivz753@gmail.com>
2017-12-11 21:03:46 -0800

    Data Structures: Array Manipulation HARD challenge completed w/ 28.57% pass rate; needs improvement on run time

--- HackerRankChallenges/src/DataStructures/ArrayManipulation.java
+++ HackerRankChallenges/src/DataStructures/ArrayManipulation.java
@@ -1,14 +1,65 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//HARD
 package DataStructures;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 /**
  *
- * @author X1 User
+ * @author Vivian Leung
  */
 public class ArrayManipulation {
+    public static int[] arrayIntegers;
+    
+    public static void printMaxInArray(){
+        int max = arrayIntegers[0];
+            for(int i: arrayIntegers){
+                if(i>max){
+                    max = i;
+                }
+            }
+        System.out.println(max);
+        }
+
+    public static int[] createIntArray(int n){
+        int[] array = new int[n];
+        //initialize every element to be zero
+        for(int i: array){
+            array[i] = 0;
+        }
+        return array;
+    }
+    
+    public static void updateArray(int a, int b, int k){
+        for(int i=a-1; i<b; i++){
+            arrayIntegers[i] += k;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //take in the n amount of elements in the array
+        int n = in.nextInt();
+        
+        //create an array of n integers
+        arrayIntegers = createIntArray(n);
+        
+        
+        
+        //take in m the amount of operations to update the array
+        int m = in.nextInt();
+        for(int a0 = 0; a0 < m; a0++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int k = in.nextInt();
+            updateArray(a, b, k);
+        }
+        in.close();
+        //System.out.println(Arrays.toString(arrayIntegers));
+        printMaxInArray();
+    }
     
 }
